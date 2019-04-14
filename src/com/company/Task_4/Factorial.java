ea9e7a4ae7962161c1129148b853dcdbaf0df3d0
lucky4ever4 <cristyapostol93@gmail.com>
2019-04-14 18:37:44 +0300

    Fixed Task_nr4: left only one version of code

--- src/com/company/Task_4/Factorial.java
+++ src/com/company/Task_4/Factorial.java
@@ -10,22 +10,7 @@ public class Factorial {
 
     public static void main(String[] args) {
 
-        System.out.println(factorialBigInteger(5));
-        System.out.println(factorialBasic(5));
-
-    }
-
-
-    public static int factorialBasic(int num) {
-
-        if(num < 0) {
-            return 1;
-        }
-        int result = 1;
-        for(int i=1; i<=num; i++) {
-            result *= i;
-        }
-        return result;
+        System.out.println(factorialBigInteger(200));
 
     }
 
